bc74692d3e8e9aac78de56279c08f216957e9c04
eric-g1g2 <eric@g1g2.xyz>
2023-10-25 20:04:25 +0800

    fix env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
-// @deno-types="https://deno.land/x/chalk_deno@v4.1.1-deno/index.d.ts"
-import chalk from "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js";
-
 import { ActionArgs } from "./action_args.ts";
 import { agent_caller } from "./agent_call.ts";
-
-function logSuccess(msg: string) {
-  console.log(chalk.green(msg));
-}
 
 export async function rebyte_main({context, args}: {
   context: any;
